746ea30b90e770f1dae55058a5916d35c0f24257
Faylixe <felix.voituret@gmail.com>
2016-05-12 18:54:01 +0200

    Fix file downloading

--- corpus-crawler.py
+++ corpus-crawler.py
@@ -16,14 +16,22 @@ GENRES = [
 	"Pop", "Rock", "Soul-RB", "Spoken"
 ]
 
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
 # Crawl the given page for the given genre.
 def crawlPage(target, page):
     document = BeautifulSoup(page, 'html.parser')
     for a in document.find_all('a', title='Download'):
         file = target + a['href'].split('/')[-1] + ".mp3"
         if not os.path.exists(file):
-            urllib.urlretrieve(a['href'], file)
-            print "\t\tDownloaded " + file
+            with open(file, 'w') as handle:
+                response = requests.get(a['href'], headers=HEADERS, stream=True)
+                if response.ok:
+                    for block in response.iter_content(1024):
+                        handle.write(block)
+                    print "\t\tDownloaded " + file
+                else:
+                    print("Error while downloading : " + a['href'])
     next = document.findAll('a', href=True, text=u'NEXT\xa0\xbb')
     if len(next) > 0:
         return next[0]['href']
@@ -37,8 +45,8 @@ def crawlGenre(output, genre):
     url = URL + genre
     while url != None:
         print "\tCrawling page " + url
-        r = requests.get(url)
-        url = crawlPage(target, r.text)
+        response = requests.get(url, headers=HEADERS)
+        url = crawlPage(target, response.text)
     return
 
 # Crawl the website.
