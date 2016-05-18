70ab722c5e91b9190758ff86a995b77d21dc55a9
Faylixe <felix.voituret@gmail.com>
2016-05-18 12:13:39 +0200

    Fix pool handler

--- neuronalnetwork.py
+++ neuronalnetwork.py
@@ -11,36 +11,25 @@ from multiprocessing import Pool
 
 import configuration
 
-NEURON_SIZE = 0
-
-def createNeuron(i):
-    """ """
-    return Neuron(NEURON_SIZE, i)
+#NEURON_SIZE = 0
+#def createNeuron(i):
+#    """ Static factory that allows Neuron to be created through multiprocessing. """
+#    return Neuron(NEURON_SIZE, i)
 
 class Neuron:
     """ Class that represents a neuron of our network. """
 
-    def __init__(self, n, id, value=1):
+    def __init__(self, size, id, value=1):
         """ Default constructor. Initializes weight list with the given value. """
         self.file = join(configuration.NEURONS_DIRECTORY, configuration.NEURONS_FILE_PREFIX + str(id))
-        weights = numpy.empty(n)
-        weights.fill(value)
-        self.save(weights)
+        weights = numpy.empty(size)
         # TODO : Add activation weight ?
-
-    def load(self):
-        """ """
-        stream = open(self.file, 'rb')
-        return pickle.load(stream)
-
-    def save(self, weights):
-        """ """
-        stream = open(self.file, 'wb')
-        pickle.dump(weights, stream, -1)
+        weights.fill(value)
+        numpy.save(self.file, weights)
 
     def apply(self, vector):
         """ Applies neuronal computation to the given vector. """
-        weights = self.load()
+        weights = numpy.load(self.file)
         result = 0 # TODO : Add activation weight ?
         for i in xrange(len(vector)):
             result = result + (vector[i] * weights[i])
@@ -49,25 +38,35 @@ class Neuron:
     def train(self, sample, alpha):
         """ Train this neuron using the given sample and alpha learning coefficient. """
         output = self.apply(sample[0])
-        weights = self.load()
+        weights = numpy.load(self.file)
         for i in xrange(len(weights)):
             weights[i] = weights[i] + (alpha * (sample[1][i] - output) * sample[0][i])
-        self.save(weights)
+        numpy.save(self.file, weights)
+
+class NeuronFactory:
+    """ A NeuronFactory is in charge of creating Neuron through a pool. """
+
+    def __init__(self, size):
+        """ Default constructor. Initializes neuron size. """
+        self.size = size
+
+    def __call__(self, id):
+        """ Factory method that creates a neuron of the target size with the given id."""
+        return Neuron(self.size, id)
 
 class NeuronalNetwork:
     """ Class that represents our neuronal network. """
 
-    def __init__(self, n):
+    def __init__(self, size):
         """ Creates a untrained neuronal network with n neurons. """
         if not exists(configuration.NEURONS_DIRECTORY):
             makedirs(configuration.NEURONS_DIRECTORY)
-        NEURON_SIZE = n
+        #NEURON_SIZE = size
+        factory = NeuronFactory(size)
         pool = Pool(configuration.THREAD)
-        self.neurons = pool.map(createNeuron, xrange(n))
+        self.neurons = pool.map(factory, xrange(size))
         pool.close()
         pool.join()
-        #for i in xrange(n):
-        #    self.neurons.append(Neuron(n, i))
 
     def train(self, corpus, alpha):
         """ Trains this network using gradient descent. """
@@ -85,8 +84,8 @@ class NeuronalNetwork:
 
     def apply(self, vector):
         """ Transforms the given vector by applying each neuron to it. """
-        n = len(self.neurons)
-        result = numpy.empty(n)
+        size = len(self.neurons)
+        result = numpy.empty(size)
         for i in xrange(n):
             result[i] = self.neurons[i].apply(vector)
         return result
